6970a4a062f6584fa75c2a94ade6ed72e0451638
gian412 <gian.regis@gmail.com>
2021-04-15 10:57:01 +0200

    build: cd

--- src/app.js
+++ src/app.js
@@ -10,5 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
+// eslint-disable-next-line
+app.use((err, req, res, next) => {
+    res.status(400).json({ error: err.stack });
+});
 
 export default app;
